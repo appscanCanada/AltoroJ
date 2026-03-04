2a6eac351babc587b63cf95be8b21033e4ffb102
rodneylive <rodneylive@users.noreply.github.com>
2026-03-04 14:32:18 -0500

    Change SQL query to use hardcoded user credentials
    Updated SQL query in DBUtil.java to use hardcoded user credentials for testing purposes.

--- src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/DBUtil.java
@@ -216,7 +216,7 @@ public class DBUtil {
 		Connection connection = getConnection();
 		Statement statement = connection.createStatement();
 		
-		ResultSet resultSet =statement.executeQuery("SELECT COUNT(*)FROM PEOPLE WHERE USER_ID = '"+ user2 +"' AND PASSWORD='" + password2 + "'"); /* BAD - user input should always be sanitized */
+		ResultSet resultSet =statement.executeQuery("SELECT COUNT(*)FROM PEOPLE WHERE USER_ID = '"+ user333 +"' AND PASSWORD='" + password333 + "'"); /* BAD - user input should always be sanitized */
 		
 		if (resultSet.next()){
 			
